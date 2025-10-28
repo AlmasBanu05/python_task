703502c52029c1fa09b03525ad736b157ea5753f
AlmasBanu05 <almasapril2005@gmail.com>
2025-10-28 17:33:59 +0530

    variables&datatypes

---
+++ variables&datatypes/vardatatypes.py
@@ -0,0 +1,30 @@
+print("==============BOI-DATA=============")
+name="Almas Banu"
+age=20
+height=140.2
+weight=45
+city="Madurai"
+is_student=False
+marks=85
+pi=3.14
+school_name={"SName":"st.Antony's grils hr.sec.school,madurai","district":"Madurai","state":"Tamilnadu"}
+my_number=10
+my_text="hello"
+print("My name is:",name)
+print("Age:",age)
+print("Height:",height)
+print("Weight:",weight)
+print("City:",city)
+print("Is_Student",is_student)
+print("Marks:",marks)
+print("Pi:",pi)
+print(f"my school name:{school_name["SName"]}")
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+print("my_number:",my_number)
+print("my_number:",type(my_number))
+print("my_text:",my_text)
+print("my_text",type(my_text))
+print("==========================================")
