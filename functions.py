5aa0f2cbc01bff16fd282d1e99ce778387afc5d0
AlmasBanu05 <almasapril2005@gamil.c0m>
2026-01-05 17:22:54 +0530

    pandas

---
+++ functions.py
@@ -0,0 +1,16 @@
+# def add(a,b):
+#     c=a+b
+#     return c
+# print(add(2,5))
+# def sample(*data):
+#    print(sum(data))
+# sample(2,3,4)
+# def sam(**data):
+#     print(data)
+# sam(name="almas",age=20)
+# def detail(**data):
+#     print(data)
+# detail(name="almas",age=26,address="pothumbu")
+def data(*tot):
+    print(len())
+data(1,2,3,4,5)
---
+++ pandas/pan_task.py
@@ -0,0 +1,28 @@
+import pandas as pd
+data=[5,10,15,20]
+df=pd.Series(data)
+print(df)
+df=pd.Series(data,index=['a','b','c','d',])
+print(df)
+print("==========Create a DataFrame=========")
+d={"name":['ravi','kiran','meena'],"age":[22,25,28],"city":['chennai','bangalore','hyderabad']}
+df=pd.DataFrame(d)
+print(df)
+print("==========Column Operations===========")
+print("============= Print only the Name column============")
+print(df["name"])
+print("===========2.	Print Name and City columns together===========")
+print(df[['name','city']])
+print("==========Row Operations==========")
+print(df[0:2])
+print("============Print only rows where Age is greater than 23============")
+print(df[df['age']>23])
+print("=========Add a New Column=========")
+df['salary']=[30000,40000,50000]
+print(df)
+print("===========Increase the Age of all people by 1==========")
+df['age']=df['age']+1
+print(df)
+print("=========Increase the Age of only Kiran by 1 using loc=========")
+df.loc[df['name'] == 'kiran', 'age'] += 1
+print(df)
